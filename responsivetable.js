4f38c375da8695e0052f300e90db1d483a33bcb9
Vadim Tomilenko <vadimto@default-value.com>
2016-10-28 17:49:17 +0300

    Table to array functionality was added.

--- responsivetable.js
+++ responsivetable.js
@@ -6,8 +6,8 @@
         var options = {
             id: 'resptable',
             class: 'resptable',
-            width: 600
-
+            width: 600,
+            removeOldTable: false
         };
 
         // update the options if needed
@@ -17,12 +17,37 @@
             })
         }
 
-        var table = $(this);
-        if (table.length) {
-            // start rebuild table process
-            this.addClass(options.id)
+        if (this.length) {
+            var tableId = this.attr('id');
+
+            var tableArray = $.map(this.find('th:not(:first)'), function(element, index) {
+                return [[$(element).text(), self.find('td:eq(' + index + ')').text()]]
+            });
+
+            if (tableArray.length) {
+                var content = '';
+
+                var tableStart = '<table id="' + options.id + '" class="' + options.class + '">'
+                var tableEnd = '</table>';
+
+                $.each(tableArray, function(index, value) {
+                    var currentTable = '';
+                    $.each(value, function(i,v) {
+                        currentTable += '<td>' + v + '</td>';
+                    })
+
+                    content += '<tr>' + currentTable + '</tr>';
+
+                });
+
+                this.after(content);
+            }
+
+            if (options.removeOldTable) {
+                this.remove();
+            }
         } else {
-            return false;
+            console.log('ResponsiveTable: Expected table is not exist!');
         }
     }
 }(jQuery));
